7fab477ec99225bdfea9443d8a78c8c57fab87c3
Albert Dong <albertdong2011@gmail.com>
2020-06-23 15:29:18 -0700

    weuhf3weiofhiowe

--- digit-recognition/model.js
+++ digit-recognition/model.js
@@ -1,3 +1,5 @@
+const answer = document.getElementById('answer');
+
 function toGrayScale() {
     i = exportData().data;
     for (a = [], u = 0; u < 3136; u += 4) a.push(i[u] / 255);
@@ -6,7 +8,7 @@ function toGrayScale() {
 
 async function recognise() {
     let probabilityModel = await tf.loadLayersModel('../model/model.json');
-    let res = probabilityModel.predict(tf.tensor([toGrayScale()]));
-    console.log(res.array());
-    return res;
+    let res = probabilityModel.predict(tf.tensor([toGrayScale()])).array();
+    console.log(res);
+    answer.innerText = res;
 }
