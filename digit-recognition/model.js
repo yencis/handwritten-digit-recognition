306745226f04e6c2ed2ca3d517169a8ad767a41f
Albert Dong <albertdong2011@gmail.com>
2020-06-23 15:35:54 -0700

    OIK IT WORKS NOW

--- digit-recognition/model.js
+++ digit-recognition/model.js
@@ -8,7 +8,19 @@ function toGrayScale() {
 
 async function recognise() {
     let probabilityModel = await tf.loadLayersModel('../model/model.json');
-    let res = probabilityModel.predict(tf.tensor([toGrayScale()])).array();
+    let res = probabilityModel.predict(tf.tensor([toGrayScale()]));
+    let data = res.dataSync()
     console.log(res);
-    answer.innerText = res;
+
+
+    let counter = 0, maxprob = 0, output;
+    while (counter < 10) {
+        if (data[counter] > maxprob) {
+            maxprob = data[counter];
+            output = counter;
+        }
+
+        counter++;
+    }
+    answer.innerText = output;
 }
