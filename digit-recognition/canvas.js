8c0ec90079e0231522a8098af5aec8f92c7cc52c
yencis <47923945+yencis@users.noreply.github.com>
2020-07-04 19:02:23 -0700

    cleaned
    cleanse

--- digit-recognition/canvas.js
+++ digit-recognition/canvas.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('canvas');
+const tempCanvas = document.getElementById('tempCanvas');
 const ctx = canvas.getContext('2d');
+const tempCtx = tempCanvas.getContext("2d");
+//const preview = document.getElementById("preview")
 let mouseX, mouseY;
 let mouseClicked = false;
 
@@ -12,16 +15,22 @@ ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 function drawPixel(ctx, x, y) {
-    let pixel = ctx.getImageData(x, y, 28, 28);
-    pixel.data[0] = 255;
+    //let pixel = ctx.getImageData(x, y, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.beginPath()
+    ctx.arc(x,y,10,0,2*Math.PI)
+    ctx.fill()
+   /* pixel.data[0] = 255;
     pixel.data[1] = 255;
     pixel.data[2] = 255;
     pixel.data[3] = 255;
-    ctx.putImageData(pixel, x, y);
+    ctx.putImageData(pixel, x, y);*/
 }
 
 function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
 }
 
 function getMousePos(e) {
@@ -44,7 +53,20 @@ function mouseMove(e) {
 }
 
 function exportData() {
-    let data = ctx.getImageData(0,0,28,28);
-    console.log(data)
-    return data
+    //let data = ctx.getImageData(0,0,canvas.width,canvas.height);
+    //console.log(data)
+    var im28x28 = new Image()
+    im28x28.src = canvas.toDataURL().toString()
+    //preview.src = canvas.toDataURL();
+   // console.log(im28x28.src)
+    im28x28.onload=function() {
+        tempCtx.drawImage(im28x28, 0, 0, 28, 28)
+
+        console.log("Image loaded")
+    };
+    //tempCtx.fillRect(0,0,28,28)
+    //tempCtx.scale(0.5,0.5)
+    //tempCtx.putImageData(data,0,0)
+    console.log("returning data")
+    //return data
 }
